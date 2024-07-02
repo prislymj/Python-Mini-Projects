9f9fc2e2420c405e4a4eb531629bd94605555700
Prisly <prislymj@gmail.com>
2024-07-02 23:45:29 +0530

    Final commit of file

--- password_manager.py
+++ password_manager.py
@@ -1,17 +1,37 @@
-master_pwd = input("What is the master password?")
+from cryptography.fernet import Fernet
+
+"""
+def write_key():
+    key = Fernet.generate_key()
+    with open("key.key", "wb") as key_file:
+        key_file.write(key)
+"""
+
+def load_key():
+    file = open("key.key","rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key() 
+fer = Fernet(key)
 
 def view():
-    pass
-
+    with open("passwords.txt","r") as f:
+        for line in f.readlines():
+            data = line.rstrip()
+            user, passw = data.split("|")
+            print("User:", user, "| Password:",
+                  fer.decrypt(passw.encode()).decode())
 def add():
     name = input("Account name: ")
     password = input("Password: ")
 
     with open("passwords.txt", 'a') as f: # 'with keyword automatically close file after use
-        f.write(name+"|"+password)
+        f.write(name+"|" + fer.encrypt(password.encode()).decode() + "\n")
 
 while True:
-    mode = input("Would you like to add a new password or view existing ones? (add/view)\n Type Q to quit").lower()
+    mode = input("Would you like to add a new password or view existing ones? (add/view)\n Type Q to quit \n").lower()
     if mode == "q":
         break
     if mode == "view":
